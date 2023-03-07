c4865376fa9fbdfd6f0ab2344649d7a64082839f
Trizzie Huynh <putmind2822@gmail.com>
2023-03-07 10:09:20 -0800

    Added code for the temp display

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,6 +1,4 @@
-var searchedCityList = [];
-var cityList = [];
-
+//Once the submit button is click, the city's temp will be rendered.
 document
   .getElementById("searchBtn")
   .addEventListener("click", function (event) {
@@ -14,10 +12,12 @@ document
         return res.json();
       })
 
-      .then(function (data) {
-        console.log(data);
-        var lat = data.city.coord.lat;
-        var lon = data.city.coord.lon;
+      .then(function (weatherData) {
+        console.log(weatherData);
+        var lat = weatherData.city.coord.lat;
+        var lon = weatherData.city.coord.lon;
+
+        fiveDaysTemp(weatherData);
 
         fetch(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`
@@ -28,7 +28,6 @@ document
           .then(function (data) {
             console.log(data);
             console.log(data.name);
-            
 
             // searchedHistory(cityName);
             displayTemp(data);
@@ -43,8 +42,9 @@ document
 //   var seached=document.createElement('p')
 // }
 
+// Render the current temperature.
 function displayTemp(data) {
-  // var date = dayjs().format("M/D/YYYY");
+  var date = new Date();
   var temp = document.createElement("div");
   temp.className = "row gy-2";
   temp.innerHTML = `<div class="card" style="width: 18rem;">
@@ -61,4 +61,71 @@ function displayTemp(data) {
             </p>
   `;
   document.getElementById("displayCard").append(temp);
+}
+
+// Render the forecast for the next 5 days
+function fiveDaysTemp(weatherData) {
+  var fiveDays = [];
+  var day1 = {
+    city: weatherData.city.name,
+    date: weatherData.list[8].dt_txt,
+    img: weatherData.list[8].weather[0].icon,
+    temp: weatherData.list[8].main.temp,
+    humidity: weatherData.list[8].main.humidity,
+    windSpeed: weatherData.list[8].wind.speed,
+  };
+  var day2 = {
+    city: weatherData.city.name,
+    date: weatherData.list[16].dt_txt,
+    img: weatherData.list[16].weather[0].icon,
+    temp: weatherData.list[16].main.temp,
+    humidity: weatherData.list[16].main.humidity,
+    windSpeed: weatherData.list[16].wind.speed,
+  };
+  var day3 = {
+    city: weatherData.city.name,
+    date: weatherData.list[24].dt_txt,
+    img: weatherData.list[24].weather[0].icon,
+    temp: weatherData.list[24].main.temp,
+    humidity: weatherData.list[24].main.humidity,
+    windSpeed: weatherData.list[24].wind.speed,
+  };
+  var day4 = {
+    city: weatherData.city.name,
+    date: weatherData.list[32].dt_txt,
+    img: weatherData.list[32].weather[0].icon,
+    temp: weatherData.list[32].main.temp,
+    humidity: weatherData.list[32].main.humidity,
+    windSpeed: weatherData.list[32].wind.speed,
+  };
+  var day5 = {
+    city: weatherData.city.name,
+    date: weatherData.list[32].dt_txt,
+    img: weatherData.list[32].weather[0].icon,
+    temp: weatherData.list[32].main.temp,
+    humidity: weatherData.list[32].main.humidity,
+    windSpeed: weatherData.list[32].wind.speed,
+  };
+
+  fiveDays.push(day1, day2, day3, day4, day5);
+  console.log(fiveDays);
+
+  fiveDays.forEach(function (day) {
+    var tempFiveDays = document.createElement("div");
+    tempFiveDays.className = "row gy-2";
+    tempFiveDays.innerHTML = `<div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 id="city" class="card-title">${day.city}</h5>
+            <h6 id="date"class="card-subtitle mb-2 text-muted">${day.date}</h6>
+            <img src="https://openweathermap.org/img/w/${day.img}.png">
+            </p>
+            <p id="temperature" class="card-text">The temperature : ${day.temp}
+            </p>
+            <p id="humidity" class="card-text">The humidity : ${day.humidity}
+            </p>
+            <p id="windSpeed" class="card-text">The wind speed : ${day.windSpeed}
+            </p>
+  `;
+    document.getElementById("fiveDays").append(tempFiveDays);
+  });
 }
